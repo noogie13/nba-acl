4e95b89060cdfb1c97e9f51a7c53e29e6105a42d
Solomon Bloch <solomonbloch@gmail.com>
2019-11-16 11:20:47 -0500

    Add spread argument for plotting. (Add more stats).

--- players.py
+++ players.py
@@ -14,19 +14,19 @@ class Player:
         self.stats = total_stats.loc[total_stats['Player'] == self.name]
         self.years_played = list(self.stats['Year'])
 
-    def one_before_after_percentage(self,stat_fn,year):
-        before = year - 1
-        after = year + 1
-        before_df = self.stats.loc[self.stats['Year'] == before]
-        after_df = self.stats.loc[self.stats['Year'] == after]
+    def before_after_percentage(self,stat_fn,year,spread):
+        before = year - spread
+        after = year + spread
+        before_df = self.stats[self.stats['Year'].between(before,year-1)]
+        after_df = self.stats[self.stats['Year'].between(year+1,after)]
 
         if before_df.empty or after_df.empty:
             return False
 
-        before_stat = stat_fn(before_df).iloc[0]
-        after_stat = stat_fn(after_df).iloc[0]
+        before_stat = stat_fn(before_df).mean()
+        after_stat = stat_fn(after_df).mean()
 
-        return (abs(before_stat - after_stat) - after_stat) / after_stat
+        return (abs(before_stat - after_stat) - before_stat) / before_stat
 
 
 def ppg(stats):
@@ -35,17 +35,26 @@ def ppg(stats):
 def apg(stats):
     return stats['AST'].div(stats['G'])
 
-def plot_stat(player_list,stat_fn):
-    stat_per = [(p.injured_years[0],p.one_before_after_percentage(stat_fn,p.injured_years[0]))
-                for p in player_list]
-    x = [x[0] for x in stat_per if x[1] and x[1] < 3]
-    y = [x[1] for x in stat_per if x[1] and x[1] < 3]
+def m(stats):
+    return stats['MP']
 
+def mpg(stats):
+    return stats['MP'].div(stats['G'])
+
+def g(stats):
+    return stats['G']
+
+def plot_stat(player_list,stat_fn,spread):
+    stat_per = [(p.injured_years[0],p.before_after_percentage(stat_fn,p.injured_years[0],spread))
+                for p in player_list]
+
+    x = [x[0] for x in stat_per if x[1] if abs(x[1] < 4)]
+    y = [x[1] for x in stat_per if x[1] if abs(x[1] < 4)]
 
     plt.plot(x,y)
     plt.plot(x,y,'or')
     plt.xlabel('Year')
-    plt.ylabel(stat_fn.__name__ + ' change')
+    plt.ylabel(stat_fn.__name__ + ' % change over '+str(spread)+' yr')
     z = numpy.polyfit(x, y, 1)
     p = numpy.poly1d(z)
     plt.plot(x,p(x),"r--")
